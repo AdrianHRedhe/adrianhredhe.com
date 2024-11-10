8585812f8430e6d34d6cb3ed5d65e2876154b220
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2024-11-10 11:44:38 +0100

    feat: add new formatting to the markdown rendering

--- app/utils/render_markdown.py
+++ app/utils/render_markdown.py
@@ -2,7 +2,36 @@
 import markdown
 import markdown.extensions.fenced_code
 import markdown.extensions.codehilite
+from pygments import formatter
+from pygments.formatters import HtmlFormatter
 from jinja2 import Template
+
+
+def format_html(html):
+
+    # General Markdown Formatter
+    #    md_css_link = '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css">'
+
+    # Custom code highlight functionality
+    formatter = HtmlFormatter(style="emacs", full=True, cssclass="codehilite")
+    css_string = formatter.get_style_defs()
+
+    # add some custom formatting
+    custom_css = """
+    .codehilite pre {
+        background-color: #f6f8fa; /* Light grey background (typical Markdown style) */
+        color: #333333; /* Dark text color for better readability */
+    }
+    .codehilite code {
+        background-color: transparent; /* No background for inline code */
+    }
+    """
+    css_string += custom_css
+    md_css_string = f"<style>{css_string}</style>"
+
+    # return md_css_link + md_css_string + html
+    return md_css_string + html
+
 
 def render_markdown(filename, variables=None):
     with open(filename, "r") as file:
@@ -10,15 +39,15 @@ def render_markdown(filename, variables=None):
         # Use jinja to render as template (can add variables)
         template = Template(markdown_string)
         rendered_md = template.render(variables or {})
-    
-        
+
     # Use markdown to convert to HTML
-    html = markdown.markdown(
-        rendered_md, 
-        extensions=["fenced_code", "codehilite"]
-    )
+    html = markdown.markdown(rendered_md, extensions=["fenced_code", "codehilite"])
 
-    return html
+    # Format HTML
+    formatted_html = format_html(html)
 
-render_markdown("README.md", {"creator": "Adrian"})
+    return formatted_html
+
+
+# render_markdown("app/templates/simpe_with_code.md", {"creator": "Adrian"})
 # %%
