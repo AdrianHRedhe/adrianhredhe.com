c397325915ae54d62738bf5125d4f6b787ca9d55
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2024-11-10 11:39:33 +0100

    feat: add new endpoint blog

--- app/main.py
+++ app/main.py
@@ -1,13 +1,15 @@
 # %%
 from fastapi import FastAPI
+from fastapi.responses import FileResponse, HTMLResponse, Response
 from pydantic import BaseModel
-from app.utils import render_markdown
+from app.utils.render_markdown import render_markdown
 
 app = FastAPI()
 
 
 class Input(BaseModel):
     name: str
+
 
 class Output(BaseModel):
     greeting: str
@@ -17,11 +19,28 @@ class Output(BaseModel):
 def home():
     return {"health_check": "OK"}
 
-@app.get("/readme")
+
+@app.get("/readme", response_class=HTMLResponse)
 def readme():
-    return render_markdown("README.md", {"Creator": "Adde"})
+    html = render_markdown("README.md", {"creator": "Adrian"})
+
+    html = render_markdown("app/templates/simpe_with_code.md")
+    return html
 
 
+@app.get("/blog/{blog_post_name}", response_class=HTMLResponse)
+def get_markdown_blog(blog_post_name):
+    html = render_markdown(f"app/templates/{blog_post_name}")
+    return html
+
+
+# @app.get("/readme2", response_class=Response)
+# def readme2():
+#     md = "README.md"
+#     with open(md, "r") as file:
+#         return Response(content=file.read(), media_type="text/markdown")
+#
+#
 @app.post("/api", response_model=Output)
 def predict(payload: Input):
     greeting = f"Hello {payload.name}"
