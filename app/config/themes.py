18f1c18fae5c3523fdc47ae88891f67dc48cd6cc
Adrian Häusler Redhe <adrian.hredhe@gmail.com>
2025-12-29 06:07:45 +0100

    feat: add support for different favicons based on themes

--- app/config/themes.py
+++ app/config/themes.py
@@ -1,9 +1,9 @@
 def get_theme(theme):
     themes = {
-        "yellow": {"accent": "#ffd166"},
-        "cyan": {"accent": "#95e6cb"},
-        "red": {"accent": "#F23C16"},
-        "blue": {"accent": "#4fc3ff"},
-        "purple": {"accent": "#bf8eff"},
+        "yellow": {"accent": "#ffd166", "favicon": "favicon-yellow.png"},
+        "cyan": {"accent": "#95e6cb", "favicon": "favicon-cyan.png"},
+        "red": {"accent": "#F23C16", "favicon": "favicon-red.png"},
+        "blue": {"accent": "#4fc3ff", "favicon": "favicon-blue.png"},
+        "purple": {"accent": "#bf8eff", "favicon": "favicon-purple.png"},
     }
     return themes.get(theme)
